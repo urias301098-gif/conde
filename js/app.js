9b33024496d37109fbd3fcae593394488a93d49c
urias301098-gif <urias301098@gmail.com>
2026-03-10 12:32:23 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 /* ==== Slider ==== */
 let slideIndex = 0;
 
+//Header moderno
+
+window.addEventListener("scroll", function(){
+let header = document.querySelector("header");
+
+if(window.scrollY > 50){
+header.classList.add("scrolled");
+}else{
+header.classList.remove("scrolled");
+}
+});
+
 // Cambiar por dot
 function goToSlide(n) {
     showSlides(slideIndex = n);
